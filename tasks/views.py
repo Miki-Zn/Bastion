4afa41b2fe6018d6da2952aa08756fb9879868fd
Michael <mznamenshchykov@gmail.com>
2026-08-07 15:36:52 +0200

    require authentication on task endpoints

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -6,3 +6,4 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
